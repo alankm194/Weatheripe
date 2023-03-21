94c3a35b0c62a0cd4ca6165c7c52fbd06808264e
Claire Tsoi <clairetsoi1129@gmail.com>
2023-03-21 17:56:37 +0000

    Add - the creation of WebClient builder in application

---
+++ src/main/java/WeathipeApplication.java
@@ -0,0 +1,18 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+public class WeathipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathipeApplication.class, args);
+	}
+
+	@Bean
+	public WebClient.Builder getWebClientBuilder(){
+		return WebClient.builder();
+	}
+
+}
