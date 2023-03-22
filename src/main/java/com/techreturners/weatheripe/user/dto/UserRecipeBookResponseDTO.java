e9e0544eac241a83c77df38b676cd9cdc125ab2b
Shanika Jajyasekara <online.shanika@gmail.com>
2023-03-22 21:46:59 +0000

    implementing Registered User save favourite recipe -testcase for happy flow

--- src/main/java/com/techreturners/weatheripe/user/dto/UserRecipeBookResponseDTO.java
+++ src/main/java/com/techreturners/weatheripe/user/dto/UserRecipeBookResponseDTO.java
@@ -3,11 +3,13 @@ package com.techreturners.weatheripe.user.dto;
 import com.techreturners.weatheripe.external.dto.ResponseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserRecipeBookResponseDTO extends ResponseDTO {
 
     List<UserRecipeBookDTO> userRecipes ;
