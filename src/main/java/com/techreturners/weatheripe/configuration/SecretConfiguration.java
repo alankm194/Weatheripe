2482e9a1e6a8bb342575c2054e900203424c45ef
alankm194 <alankm194@gmail.com>
2023-03-23 16:02:35 +0000

    refactor: make properties secret configuration class to hold secrets for external API services
    - secrets read from secret.properties local to developer
    - removed properties from properties files

--- src/main/java/com/techreturners/weatheripe/configuration/SecretConfiguration.java
+++ src/main/java/com/techreturners/weatheripe/configuration/SecretConfiguration.java
@@ -3,5 +3,7 @@ package com.techreturners.weatheripe.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("secretkey")
-public record SecretConfiguration(String jwtKey, String weatherApiKey, String recipeAppKey, String recipeAppId, String recipeApiType) {
+public record SecretConfiguration(String weatherApiKey,
+                                  String recipeAppKey,
+                                  String recipeAppId) {
 }
