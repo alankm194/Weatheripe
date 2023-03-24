02c7e27f27d1221dd5cf9bc4a010328b9b4f7146
jimdeal <jimdeal@hotmail.com>
2023-03-24 19:17:26 +0000

    take out unused imports

--- src/main/java/com/techreturners/weatheripe/dish/DishService.java
+++ src/main/java/com/techreturners/weatheripe/dish/DishService.java
@@ -1,8 +1,6 @@
 package com.techreturners.weatheripe.dish;
 
 import com.techreturners.weatheripe.model.DishType;
-import com.techreturners.weatheripe.external.dto.ResponseDTO;
-import com.techreturners.weatheripe.model.Weather;
 
 import java.util.List;
 
--- src/main/java/com/techreturners/weatheripe/repository/DishTypeRepository.java
+++ src/main/java/com/techreturners/weatheripe/repository/DishTypeRepository.java
@@ -2,7 +2,6 @@ package com.techreturners.weatheripe.repository;
 
 import com.techreturners.weatheripe.model.DishType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
