648aee43d8929bfd71a8df2053ac17283f52fa4d
alankm194 <alankm194@gmail.com>
2023-03-24 10:58:52 +0000

    test: add testing framework for testing JWTs
    - refactored Erole into PermissionRole

--- src/test/java/com/techreturners/weatheripe/controller/RecipeControllerTests.java
+++ src/test/java/com/techreturners/weatheripe/controller/RecipeControllerTests.java
@@ -1,11 +1,15 @@
 package com.techreturners.weatheripe.controller;
 
+import com.c4_soft.springaddons.security.oauth2.test.annotations.OpenIdClaims;
+import com.c4_soft.springaddons.security.oauth2.test.annotations.WithMockJwtAuth;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techreturners.weatheripe.exception.ExceptionHandlerController;
 import com.techreturners.weatheripe.external.dto.ResponseDTO;
 import com.techreturners.weatheripe.recipe.dto.RecipeResponseDTO;
 import com.techreturners.weatheripe.recipe.service.RecipeServiceImpl;
 import com.techreturners.weatheripe.user.UserAccountServiceImpl;
+import com.techreturners.weatheripe.user.dto.UserRecipeBookDTO;
+import com.techreturners.weatheripe.user.dto.UserRecipeBookResponseDTO;
 import com.techreturners.weatheripe.weather.dto.RecipeQueryDTO;
 import com.techreturners.weatheripe.weather.dto.WeatherApiDTO;
 import com.techreturners.weatheripe.weather.service.WeatherServiceImpl;
@@ -16,12 +20,21 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.core.Is.is;
+
+import java.security.Principal;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -43,13 +56,12 @@ public class RecipeControllerTests {
 
     private ObjectMapper mapper;
 
-
     @BeforeEach
     public void setup() {
         mockMvcController = MockMvcBuilders.standaloneSetup(recipeController)
-                .setControllerAdvice(new ExceptionHandlerController()).build();
+                .setControllerAdvice(new ExceptionHandlerController())
+                .build();
         mapper = new ObjectMapper();
-
     }
 
     @Test
@@ -65,7 +77,7 @@ public class RecipeControllerTests {
         when(mockRecipeServiceImpl.getRecipeByWeatherCondition(recipeQueryDTO)).thenReturn(recipeResponseDTO);
 
         this.mockMvcController.perform(
-                        MockMvcRequestBuilders.get("/api/v1/recipe/"+location))
+                        MockMvcRequestBuilders.get("/api/v1/recipe/" + location))
                 .andExpect(MockMvcResultMatchers.status().isOk());
         // TODO: add more expect values when the recipeAPI is completed
 //                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
@@ -75,19 +87,33 @@ public class RecipeControllerTests {
 //TODO Fix Test
 
 
-//    @Test
-//    public void testGetWeatherByLocationForUser() throws Exception {
-//        String location = "London";
-//        UserRecipeBookResponseDTO  userRecipeBookResponseDTO = new UserRecipeBookResponseDTO();
-//
-//        when(mockWeatherServiceImpl.getRecipeByLocationForUser(location,"")).thenReturn(userRecipeBookResponseDTO);
-//        this.mockMvcController.perform(
-//                        MockMvcRequestBuilders.get("/api/v1/recipe/user/"+location))
-//                .andExpect(MockMvcResultMatchers.status().isCreated());
-//
-//    }
+    @Test
+    public void testGetWeatherByLocationForUser() throws Exception {
+        Principal mockPrincipal = mock(Principal.class);
+        String username = "username";
+        when(mockPrincipal.getName()).thenReturn(username);
 
+        String location = "London";
+        UserRecipeBookResponseDTO userRecipeBookResponseDTO = new UserRecipeBookResponseDTO();
 
+        userRecipeBookResponseDTO.setUserRecipes(List.of(UserRecipeBookDTO.builder()
+                .recipeBookId(1L)
+                .recipe("myRecipe")
+                .url("url.recipe")
+                .dishType("myDish")
+                .build()))
+        ;
 
-
+        when(mockWeatherServiceImpl.getRecipeByLocationForUser(location, username)).thenReturn(userRecipeBookResponseDTO);
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders
+                                .get("/api/v1/recipe/user/" + location)
+                                .principal(mockPrincipal)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(jsonPath("$.userRecipes.[0].recipeBookId", is(1)))
+                .andExpect(jsonPath("$.userRecipes.[0].recipe", is("myRecipe")))
+                .andExpect(jsonPath("$.userRecipes.[0].url", is("url.recipe")))
+                .andExpect(jsonPath("$.userRecipes.[0].dishType", is("myDish")));
+    }
 }
