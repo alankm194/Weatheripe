5c83077d697c2c95dcd7dca96a19cfdb5300fcd3
Shanika Jajyasekara <online.shanika@gmail.com>
2023-03-25 16:34:35 +0000

    added swagger related anotations

--- src/main/java/com/techreturners/weatheripe/exception/weather/NoMatchingWeatherException.java
+++ src/main/java/com/techreturners/weatheripe/exception/weather/NoMatchingWeatherException.java
@@ -3,7 +3,7 @@ package com.techreturners.weatheripe.exception.weather;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.EXPECTATION_FAILED)
 public class NoMatchingWeatherException extends RuntimeException{
     public NoMatchingWeatherException(String message) {
         super(message);
