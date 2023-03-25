e722116588226a9c3741e17b624f25978a91fe46
alankm194 <alankm194@gmail.com>
2023-03-25 12:16:38 +0000

    fix: 401 with no response happens when inputting username or password which violate validation restrictions. Now returns proper error response body and status.
    - change AuthController to use created instead of OK response

--- src/main/java/com/techreturners/weatheripe/exception/ExceptionHandlerController.java
+++ src/main/java/com/techreturners/weatheripe/exception/ExceptionHandlerController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -120,16 +121,26 @@ public class ExceptionHandlerController {
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponseDTO> handleLoginResourceAlreadyExistsException(LoginResourceAlreadyExistsException ex, WebRequest request) {
         return new ResponseEntity<>(
-               new ErrorResponseDTO(ex.getMessage(),"W0012", "REGISTER_DETAILS_INVALID", "REGISTER"),
+               new ErrorResponseDTO(ex.getMessage(),"W0012", "DETAILS_INVALID", "REGISTER"),
         HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = {BadCredentialsException.class})
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponseDTO> handleResourceAlreadyExistsException(BadCredentialsException ex, WebRequest request) {
-        return new ResponseEntity<>(new ErrorResponseDTO(ex.getMessage(), "W0013", "BAD_CREDENTIALS", "LOGIN"),
+        return new ResponseEntity<>(new ErrorResponseDTO(ex.getMessage(), "W0013", "BAD_CREDENTIALS", "AUTH"),
         HttpStatus.NOT_FOUND);
 
     }
 
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+        String errorResp = "Invalid arguments used during registration";
+        if (ex.getErrorCount() > 0) {
+            errorResp = ex.getAllErrors().get(0).getDefaultMessage();
+        }
+        return new ResponseEntity<>(new ErrorResponseDTO(errorResp, "W0014", "DETAILS_INVALID", "AUTH"),
+                HttpStatus.BAD_REQUEST);
+    }
 }
